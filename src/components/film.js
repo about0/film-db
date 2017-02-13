32d5bc383f0dda919e9e09294010e93117a06bd6
about0 <ghostautomatic@gmail.com>
2017-02-13 17:28:42 +0200

    added fome dummy info to the json file

---
+++ src/components/film.js
@@ -0,0 +1,26 @@
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
+
+class FilmDetails extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cover: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+FilmDetails.propTypes = {};
+FilmDetails.defaultProps = {};
+
+export default FilmDetails;
