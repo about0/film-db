a2815688a5d0aed5f2bc982b63d95151db48ef14
about0 <ghostautomatic@gmail.com>
2017-02-13 17:50:04 +0200

    Changed a bit structure of json dummy file to follow the best practices :)

--- src/components/film.js
+++ src/components/film.js
@@ -2,19 +2,34 @@ import React, {
   Component,
   PropTypes,
 } from 'react';
+import Axios from 'axios';
 
 class FilmDetails extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       cover: ''
     }
   }
 
+  componentDidMount() {
+    this.serverRequest = Axios.get('https://raw.githubusercontent.com/about0/film-db/master/src/data/dummyFilms.json')
+      .then(result => {
+        this.setState({
+          cover: result.data[0].cover_image
+        },
+        console.log(result.data[0]))
+      })
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort()
+  }
+
   render() {
     return (
       <div>
-
+        <img src={this.state.cover} alt=""/>
       </div>
     );
   }
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import {storiesOf, action, linkTo} from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
+import FilmDetails from '../components/film';
 
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
@@ -14,4 +15,9 @@ storiesOf('Button', module)
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf('Film Details', module)
+  .add('Cover', () => (
+    <FilmDetails />
   ));
